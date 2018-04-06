cee4c3a6c7644397ca176b611a6c2ecbe012a9d7
John Linneman <urielpro78@gmail.com>
2018-04-06 13:30:10 -0700

    add forms for sauce, size, and toppings and add variables to get values

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// user logic
+
+$(function() {
+  event.preventDefault();
+  var whatSauce = parseFloat($("select#user-sauce").val())
+  var whatSize = parseFloat($("select#user-size").val())
+  var whatTopping = $("select#user-toppings").val()
+});
