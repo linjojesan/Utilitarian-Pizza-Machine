afd9105b1adb23d5fccaf12888f877441514b496
John Linneman <urielpro78@gmail.com>
2018-04-06 16:57:24 -0700

    add readme file and tested program, final submit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Utilitarian Pizza Machine
+
+## Final Epicodus Project- Copyright (c) 2017 John Linneman
+### MIT License 2018
+
+### Description
+
+ Utilitarian Pizza Machine is a website that enables you to order a pizza. It's an exercise in constructing objects and prototypes. The goal is to allow the user to order a complete pizza and receive pricing information and the details of his or her order.
+
+# Specs
+| Spec | Input | Output |
+| :-------------     | :------------- | :------------- |
+| **User selects a pizza sauce | User Input: "Tomato sauce"| Output: "Tomato Sauce and the program holds the items sauce and cost in an object| Input: Tomato Sauce| Output: .50 is added to the total and tomato sauce is added to the total"|
+| **User selects another item and that items cost is added to the other item| Input: "pizza size large" | output: "$15.00 is added to total" |
+| ** Program calculates cost and displays the total" | Input: "Large pizza, tomato sauce, gudda cheese, sausage, pepperoni" | Output: "Here is your order: tomato, large, sausage, gudda" "The total will be $16.50 "|
+
+# Technologies
+js, html, css, jquery
+
+## Set up
+Please clone the repository and open the index.html file in your browser.
--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 
   // Business logic
 
-// Object to store Pizza Order
-function PizzaDetails(pizzaSize,pizzaSauce,pizzaToppings,pizzaCheeses) {
+// Objects for pizz details adn pizza
+
+  function PizzaDetails(pizzaSize,pizzaSauce,pizzaToppings,pizzaCheeses) {
     this.pizzaSize = pizzaSize;
     this.pizzaSauce = pizzaSauce;
     this.pizzaToppings = pizzaToppings;
     this.pizzaCheeses = pizzaCheeses;
   }
 
-  // object to store pizza total
   function PizzaTotal(size,sauce,toppings,cheeses) {
   this.size = size;
   this.sauce = sauce;
@@ -17,20 +17,12 @@ function PizzaDetails(pizzaSize,pizzaSauce,pizzaToppings,pizzaCheeses) {
   this.cheeses = cheeses;
 }
 
-
   // prototype to calculate cost
 
-PizzaTotal.prototype.calculateCost = function() {
+  PizzaTotal.prototype.calculateCost = function() {
     var totalCost = (this.size) + (this.sauce) + (this.toppings) + (this.cheeses);
     return totalCost;
   }
-
-
-
-
-
-
-
 
 // user logic
 
@@ -38,12 +30,16 @@ $(function() {
   $("form#pizza-style").submit(function(event) {
   event.preventDefault();
 
-
+  // variables for user values, using map, and reduce to add values and return sum, instead af a for loop.
+  // https://hackernoon.com/understanding-map-filter-and-reduce-in-javascript-5df1c7eee464https://hackernoon.com/understanding-map-filter-and-reduce-in-javascript-5df1c7eee464
   var whatSauce = parseFloat($("select#user-sauce").val())
   var whatSize = parseFloat($("select#user-size").val())
-  var whatTopping = $("select#user-toppings").val()
-  var whatCheese = $("select#user-cheeses").val()
-    });
+  var whatTopping = $("select#user-toppings").val().map(parseFloat).reduce(function (sum,b) {
+    return sum + b
+  });
+  var whatCheese = $("select#user-cheeses").val().map(parseFloat).reduce(function (sum,b) {
+    return sum + b
+  });
 
   // variables for order cost
   var newPizzaTotal = new PizzaTotal(whatSize, whatSauce, whatTopping, whatCheese)
@@ -51,9 +47,14 @@ $(function() {
   var sizeText = $("select#user-size option:selected").text();
   var toppingText = $("select#user-toppings option:selected").text();
   var CheeseText = $("select#user-cheeses option:selected").text();
+  // variables for
 
 
-
-
+  var newPizzaDetails = new PizzaDetails(sauceText, sizeText, toppingText, CheeseText)
+  $("#order, #total").show();
+  // calculates cost method on newPizzaTotal object
+    $("#total-amount").text(newPizzaTotal.calculateCost().toFixed(2));
+    // returns cost with object.values method
+    $("#order-type").text(Object.values(newPizzaDetails).join(", "));
 });
 });
