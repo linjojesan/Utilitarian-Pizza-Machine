656240ba85c4415b142f4c5c8e3bb8d4c42a8c83
John Linneman <urielpro78@gmail.com>
2018-04-06 16:21:15 -0700

    finish html infastructure

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,15 @@
 
   // Business logic
 
-// Objects for pizz details adn pizza
-function Pizza(pizzaSize,pizzaSauce,pizzaToppings,pizzaCheeses) {
+// Object to store Pizza Order
+function PizzaDetails(pizzaSize,pizzaSauce,pizzaToppings,pizzaCheeses) {
     this.pizzaSize = pizzaSize;
     this.pizzaSauce = pizzaSauce;
     this.pizzaToppings = pizzaToppings;
     this.pizzaCheeses = pizzaCheeses;
   }
 
+  // object to store pizza total
   function PizzaTotal(size,sauce,toppings,cheeses) {
   this.size = size;
   this.sauce = sauce;
@@ -16,13 +17,9 @@ function Pizza(pizzaSize,pizzaSauce,pizzaToppings,pizzaCheeses) {
   this.cheeses = cheeses;
 }
 
-function PizzaTotal(size,sauce,toppings,cheeses) {
-  this.size = size;
-  this.sauce = sauce;
-  this.toppings = toppings;
-  this.cheeses = cheeses;
-}
+
   // prototype to calculate cost
+
 PizzaTotal.prototype.calculateCost = function() {
     var totalCost = (this.size) + (this.sauce) + (this.toppings) + (this.cheeses);
     return totalCost;
@@ -40,17 +37,22 @@ PizzaTotal.prototype.calculateCost = function() {
 $(function() {
   $("form#pizza-style").submit(function(event) {
   event.preventDefault();
+
+
   var whatSauce = parseFloat($("select#user-sauce").val())
   var whatSize = parseFloat($("select#user-size").val())
   var whatTopping = $("select#user-toppings").val()
-  var whatCheese = $("select#user-cheeses").val().map(parseFloat)
+  var whatCheese = $("select#user-cheeses").val()
+    });
 
+  // variables for order cost
+  var newPizzaTotal = new PizzaTotal(whatSize, whatSauce, whatTopping, whatCheese)
   var sauceText = $("select#user-sauce option:selected").text();
   var sizeText = $("select#user-size option:selected").text();
   var toppingText = $("select#user-toppings option:selected").text();
   var CheeseText = $("select#user-cheeses option:selected").text();
 
-  var newPizzaTotal = new PizzaTotal(whatSize, whatSauce, whatTopping, whatCheese)
+
 
 
 });
