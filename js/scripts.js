06be98525b9f4921b7fb3e81bce0f7efbb54c16b
John Linneman <urielpro78@gmail.com>
2018-04-09 00:31:04 -0700

    added more comments for code logic

--- js/scripts.js
+++ js/scripts.js
@@ -17,7 +17,8 @@
   this.cheeses = cheeses;
 }
 
-  // prototype to calculate cost
+
+  // prototype of Pizzatotal which adds the parameters of the function pizza total and returns the total cost
 
   PizzaTotal.prototype.calculateCost = function() {
     var totalCost = (this.size) + (this.sauce) + (this.toppings) + (this.cheeses);
@@ -31,20 +32,20 @@ $(function() {
   event.preventDefault();
 
   // variables for user values, using map, and reduce to add values and return sum, instead af a for loop.
-  // https://hackernoon.com/understanding-map-filter-and-reduce-in-javascript-5df1c7eee464https://hackernoon.com/understanding-map-filter-and-reduce-in-javascript-5df1c7eee464
+  // https://hackernoon.com/understanding-map-filter-and-reduce-in-javascript-5df1c7eee464
   var whatSauce = parseFloat($("select#user-sauce").val())
   var whatSize = parseFloat($("select#user-size").val())
-  var whatTopping = $("select#user-toppings").val().map(parseFloat).reduce(function (sum,b) {
-    return sum + b
+  var whatTopping = $("select#user-toppings").val().map(parseFloat).reduce(function (toppingCombo,toppingTotal) {
+    return toppingCombo + toppingTotal;
   });
-  var whatCheese = $("select#user-cheeses").val().map(parseFloat).reduce(function (sum,b) {
-    return sum + b
+  var whatCheese = $("select#user-cheeses").val().map(parseFloat).reduce(function (cheeseCombo,cheeseTotal) {
+    return cheeseCombo + cheeseTotal;
   });
 
-  // variables for order cost
+  // newPizzaTotal object has four parameters whatSize, whatSauce, WhatTopping, whatCheese and outputs the pizza order.
   var newPizzaTotal = new PizzaTotal(whatSize, whatSauce, whatTopping, whatCheese)
-  var sauceText = $("select#user-sauce option:selected").text();
-  var sizeText = $("select#user-size option:selected").text();
+  var sizeText = $("select#user-sauce option:selected").text();
+  var sauceText = $("select#user-size option:selected").text();
   var toppingText = $("select#user-toppings option:selected").text();
   var CheeseText = $("select#user-cheeses option:selected").text();
   // variables for
@@ -52,7 +53,7 @@ $(function() {
 
   var newPizzaDetails = new PizzaDetails(sauceText, sizeText, toppingText, CheeseText)
   $("#order, #total").show();
-  // calculates cost method on newPizzaTotal object
+  // text's total amount of the pizza, by using calculateCost function
     $("#total-amount").text(newPizzaTotal.calculateCost().toFixed(2));
     // returns cost with object.values method
     $("#order-type").text(Object.values(newPizzaDetails).join(", "));
